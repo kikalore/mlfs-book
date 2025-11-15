e491b34e0d84fa5c3c3d0b5f9fd972b9ff979b9a
Alex <wikialex02@gmail.com>
2025-11-15 22:53:41 +0100

    task C completed

--- mlfs/airquality/util.py
+++ mlfs/airquality/util.py
@@ -298,3 +298,18 @@ def backfill_predictions_for_monitoring(weather_fg, air_quality_df, monitor_fg, 
     df = df.drop('pm25', axis=1)
     monitor_fg.insert(df, write_options={"wait_for_job": True})
     return hindcast_df
+
+def backfill_predictions_for_monitoring_lagged(weather_fg, air_quality_df, monitor_fg, model):
+    features_df = weather_fg.read()
+    features_df = features_df.sort_values(by=['date'], ascending=True)
+    features_df = features_df.tail(10)
+
+    features_df = pd.merge(features_df, air_quality_df[['date','pm25_lagged_1', 'pm25_lagged_2', 'pm25_lagged_3']], on="date")
+
+    features_df['predicted_pm25'] = model.predict(features_df[['pm25_lagged_1', 'pm25_lagged_2', 'pm25_lagged_3', 'temperature_2m_mean', 'precipitation_sum', 'wind_speed_10m_max', 'wind_direction_10m_dominant']])
+    df = pd.merge(features_df, air_quality_df[['date','pm25','street','country']], on="date")
+    df['days_before_forecast_day'] = 1
+    hindcast_df = df
+    df = df.drop('pm25', axis=1)
+    monitor_fg.insert(df, write_options={"wait_for_job": True})
+    return hindcast_df
